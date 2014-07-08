389c01bbce0f2b236eae09aaae73c84b13278a20
Vasily Golubev <w.golubev@mail.ru>
2014-07-08 18:02:03 +0400

    [src] Fix test for migration.

--- src/tests/rect_borders.py
+++ src/tests/rect_borders.py
@@ -4,6 +4,9 @@ c = 2000
 dt = 0.01
 nt = 230
 # width of impulse 400
+width = 400.0
+l_1 = 500.0
+l_2 = 1000.0
 dx = dy = dz = 20
 ny = 1
 nx = nz = 75
@@ -13,15 +16,15 @@ fd.write(
 	"# N_STEPS\n# N_X\n# N_Y\n# N_Z\n# DT\n# H_X\n# H_Y\n# H_Z\n# P(i,j) \\tab P(i+1,j) ... \\tab P(i,j+1) \\tab P(i+1,j+1)\n"
 	)
 
-fd.write("230\n75\n1\n75\n0.01\n20\n20\n20\n");
+fd.write("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n".format(nt, nx, ny, nz, dt, dx, dy, dz));
 
 # Before response.
-for t in range(0, 50):
+for t in range(0, int(2 * l_1 / c / dt)):
 	for j in range(0, ny):
 		for i in range(0, nx):
 			fd.write("0.0\t")
 	fd.write("\n")
-for t in range(50, 70):
+for t in range(50, int(2 * l_1 / c / dt + width / c / dt)):
 	for j in range(0, ny):
 		for i in range(0, nx):
 			if (i < nx / 3):
@@ -29,12 +32,12 @@ for t in range(50, 70):
 			else:
 				fd.write("0.0\t")
 	fd.write("\n")
-for t in range(70, 100):
+for t in range(int(2 * l_1 / c / dt + width / c / dt), int(2 * l_2 / c / dt)):
 	for j in range(0, ny):
 		for i in range(0, nx):
 			fd.write("0.0\t")
 	fd.write("\n")
-for t in range(100, 120):
+for t in range(int(2 * l_2 / c / dt), int(2 * l_2 / c / dt + width / c / dt)):
 	for j in range(0, ny):
 		for i in range(0, nx):
 			if (i > nx / 3):
@@ -42,7 +45,7 @@ for t in range(100, 120):
 			else:
 				fd.write("0.0\t")
 	fd.write("\n")
-for t in range(120, nt):
+for t in range(int(2 * l_2 / c / dt + width / c / dt), nt):
 	for j in range(0, ny):
 		for i in range(0, nx):
 			fd.write("0.0\t")
